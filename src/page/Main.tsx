9c74b7e8967559952b0271a1851bbc7ad5a7da5c
minfan Chen <mingfan1202@gmail.com>
2024-12-04 23:57:59 +0800

    feat: 作品

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom'
 import 'bootstrap/dist/js/bootstrap.js'
 import '@assets/bootstrap.scss';
@@ -9,14 +9,28 @@ export default function Main() {
   const NAV_LINK = [
     { name: 'ABOUT', link: '#About' },
     { name: 'PORTFOLIO', link: '#Portfolio' },
+    { name: 'WEB', link: '/web' },
+    { name: 'WEB DESIGN', link: '/webDesign' },
+    { name: 'GRAPHIC DESIGN', link: '/graphic' },
     { name: 'WORK EXPERIENCE', link: '#Work' }
   ]
-  const [isNav, setIsNav] = useState('');
+  const [scrollHeight, setScrollHeight] = useState(0)
+  const scrollToAnchor = (hashName) => {
+    let scrollDOM = document.getElementById(hashName.link.substring(1))
+    setScrollHeight(scrollDOM?.offsetTop || 0)
+  }
+
+  useEffect(() => {
+    if (scrollHeight !== 0) {
+      window.scrollTo(0, scrollHeight - 56)
+    }
+  }, [scrollHeight])
+
   return (
     <>
-      <Header NAV_LINK={NAV_LINK} />
+      <Header NAV_LINK={NAV_LINK} scrollToAnchor={scrollToAnchor} />
       <main>
-        <Outlet context={{ NAV_LINK }} />
+        <Outlet context={{ NAV_LINK, scrollHeight }} />
       </main>
       <Footer />
     </>
