98d7637a7145b2dfeccf2816c52c3f53cbb8e2f7
minfan Chen <mingfan1202@gmail.com>
2024-12-04 23:57:59 +0800

    feat: 作品

--- src/components/common/LazyLoadImage.tsx
+++ src/components/common/LazyLoadImage.tsx
@@ -5,17 +5,18 @@ type LazyLoadImgType = {
   className: string,
   alt: string,
   src: string,
+  placeholdersrc?: string,
   width?: number,
   height?: number,
 }
 const LazyLoadImg = (props: LazyLoadImgType) => {
-  const { alt, src, width = 800, height = 500, className, } = props
+  const { alt, src, width = 800, height = 500, className, placeholdersrc } = props
   return (
     <LazyLoadImage
       alt={alt}
       src={src} // use normal <img> attributes as props
       className={className}
-      placeholdersrc="https://plus.unsplash.com/premium_photo-1707931342773-6eb567d4fd21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z3JleXxlbnwwfHwwfHx8MA%3D%3D"
+      placeholdersrc={placeholdersrc}
       effect="blur"
       visibleByDefault={src === 'https://plus.unsplash.com/premium_photo-1707931342773-6eb567d4fd21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z3JleXxlbnwwfHwwfHx8MA%3D%3D'}
     />
