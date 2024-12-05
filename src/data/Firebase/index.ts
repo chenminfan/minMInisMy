c637b33e60d4b0fd8be0b673a2d150bb11d38f0c
minfan Chen <mingfan1202@gmail.com>
2024-12-05 13:14:35 +0800

    feat: 作品

--- src/data/Firebase/index.ts
+++ src/data/Firebase/index.ts
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
@@ -17,5 +11,4 @@ export const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
