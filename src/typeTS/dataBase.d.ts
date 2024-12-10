298035bedccb3b391a4e43e49780f45f4df55b1d
minfan Chen <mingfan1202@gmail.com>
2024-12-10 23:41:20 +0800

    feat: firebase資料庫修正

---
+++ src/typeTS/dataBase.d.ts
@@ -0,0 +1,31 @@
+export type infoProps = {
+  title?: string | null;
+  content?: string | null;
+  category?: string | null;
+  time?: string | null;
+  imageUrl?: string | null;
+}
+export type content1Props = {
+  courses?: string;
+  coursesLink?: string;
+}
+export type WORKProps = {
+  work: string,
+  workName: string,
+  endTime: string,
+  startTime: string,
+  workTitle: string,
+  workTool: string | null,
+  infoTitle: string,
+  info: infoProps[],
+  subInfo?: {
+    subInfoTitle?: string;
+    content?: string;
+    content1?: content1Props[];
+  };
+}
+
+export type DATABASEProps = {
+  work: { WORKProps },
+  portfolio: { string },
+}
