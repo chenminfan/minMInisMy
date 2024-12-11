a082104ed9cf3e5e8abc1b8e8cc33f514c8b3f16
minfan Chen <mingfan1202@gmail.com>
2024-12-11 19:03:10 +0800

    feat: 作品卡片元件化

--- src/components/home/Portfolio/index.tsx
+++ src/components/home/Portfolio/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useOutletContext } from 'react-router-dom';
-import LazyLoadImg from "@components/common/LazyLoadImage";
+import CardInfo from "@components/common/CardInfo";
 import { DATABASEProps } from '@typeTS/dataBase'
 import './portfolio.scss'
 
@@ -8,6 +8,7 @@ type dataType = {
   myDataBase: DATABASEProps
 }
 export default function Portfolio() {
+
   const { myDataBase } = useOutletContext<dataType>();
   const sortOrder = ["React網站開發", "React切版", "html網站切版", "網站設計", "一頁式網頁設計"];
   const PORTFOLIO = Object.values(myDataBase.portfolio || {}).filter((workItem) => workItem.category).sort((a: any, b: any) => {
@@ -17,7 +18,6 @@ export default function Portfolio() {
   })
   const [categoryId, setCategoryId] = useState<string>('all')
   const PORTFOLIO_NAV = Array.from(new Set(PORTFOLIO.map((category) => category.category)))
-  const PORTFOLIO_NAV_ID = PORTFOLIO_NAV.find((item) => item)
   const handleNavClick = (nav) => {
     setCategoryId(nav)
   }
@@ -42,36 +42,19 @@ export default function Portfolio() {
                       <button className={`btn btn-outline-primary ${categoryId === 'all' ? 'active' : ''}`} type="button" onClick={() => { handleNavClick('all') }}>WebAll</button>
 
                       {PORTFOLIO_NAV.slice(0, 3).map((navItem) => (
-                        <button key={`nev_${navItem}`} className={`btn btn-outline-primary ${navItem === (categoryId ? categoryId : PORTFOLIO_NAV_ID) ? 'active' : ''}`} type="button" onClick={() => { handleNavClick(navItem) }}>{navItem}</button>
+                        <button key={`nev_${navItem}`} className={`btn btn-outline-primary ${navItem === categoryId ? 'active' : ''}`} type="button" onClick={() => { handleNavClick(navItem) }}>{navItem}</button>
                       ))}
                     </div>
                   </div>
                 </nav>
-                <div className="portfolio-content">
-                  {PORTFOLIO_CATEGORY.map((item, index) => (
-                    <div className="portfolio-card" key={`portfolio_${index}`}>
-                      <a href="/#" className="portfolio-link">
-                        <div className="portfolio-title"><div className="portfolio-title-text">{item.title}</div></div>
-                        {item.imageUrl && (
-                          <div className={`portfolio-image ${item.category === "一頁式網頁設計" ? 'portfolio-image-webPage' : ''}`}>
-                            <div className="img-box">
-                              <LazyLoadImg className="" src={require(`../../../assets/image/Portfolio/${item.imageUrl}`)} alt={item.imageUrl} />
-                            </div>
-                            <div className='portfolio-info'>
-                              <div className="portfolio-info-tag">{item.category}</div>
-                              <div className="portfolio-info-text">{(item.workTitle).match('Student') ? "學生作品" : item.workTitle}</div>
-                            </div>
-                          </div>
-                        )}
 
-                      </a>
-                      <div className="portfolio-tool">
-                        {item.link && (<a className="btn" href={item.link}><span className="material-symbols-outlined">
-                          open_in_new
-                        </span></a>)}
-                      </div>
-                    </div>
-                  )
+                <div className="portfolio-content">
+
+                  {PORTFOLIO_CATEGORY.map((item, index) => {
+                    return (
+                      <CardInfo card={item} key={`portfolio_${index}`}></CardInfo>
+                    )
+                  }
                   )}
                 </div>
               </div>
