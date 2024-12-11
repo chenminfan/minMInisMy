fd88bea7a7881c3697fea80da9148606f4b7c58a
minfan Chen <mingfan1202@gmail.com>
2024-12-12 03:52:24 +0800

    feat: 作品卡片優化及整合btnGroupNav
    feat: 作品卡片優化及整合btnGroupNav

--- src/components/home/Portfolio/index.tsx
+++ src/components/home/Portfolio/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useOutletContext } from 'react-router-dom';
+import BtnGroupNav from "@components/common/BtnGroupNav";
 import CardInfo from "@components/common/CardInfo";
 import { DATABASEProps } from '@typeTS/dataBase'
 import './portfolio.scss'
@@ -10,24 +11,23 @@ type dataType = {
 export default function Portfolio() {
 
   const { myDataBase } = useOutletContext<dataType>();
-  const sortOrder = ["React網站開發", "React切版", "html網站切版", "網站設計", "一頁式網頁設計"];
-  const PORTFOLIO = Object.values(myDataBase.portfolio || {}).filter((workItem) => workItem.category).sort((a: any, b: any) => {
-    if (!sortOrder.includes(a.category)) return 1
-    if (!sortOrder.includes(b.category)) return -1
-    return sortOrder.indexOf(a.category) - sortOrder.indexOf(b.category)
+  const PAGE_KEY_WORD = 'WEB ALL'
+  const ORDER = ["REACT網站開發", "REACT切版", "HTML網站切版", "網站設計"];
+  const PORTFOLIO_BASE = Object.values(myDataBase.portfolio || {})
+  const PORTFOLIO = PORTFOLIO_BASE.filter((workItem) => workItem.category).sort((a: any, b: any) => {
+    if (!ORDER.includes(a.category)) return 1
+    if (!ORDER.includes(b.category)) return -1
+    return ORDER.indexOf(a.category) - ORDER.indexOf(b.category)
   })
-  const [categoryId, setCategoryId] = useState<string>('all')
-  const PORTFOLIO_NAV = Array.from(new Set(PORTFOLIO.map((category) => category.category)))
-  const handleNavClick = (nav) => {
-    setCategoryId(nav)
-  }
+  console.log(PORTFOLIO)
+  const [categoryId, setCategoryId] = useState<string>(PAGE_KEY_WORD)
   const PORTFOLIO_CATEGORY = PORTFOLIO.filter((item) => {
-    if (categoryId !== 'all') {
+    if (categoryId !== PAGE_KEY_WORD) {
       return item.category.match(categoryId)
     } else {
       return item.category
     }
-  }).filter((a) => PORTFOLIO_NAV.slice(0, 3).includes(a.category))
+  })
   return (
     <div className="container-fluid">
       <div className="row">
@@ -38,24 +38,16 @@ export default function Portfolio() {
               <div className="portfolio-box">
                 <nav className="portfolio-navbar">
                   <div className="portfolio-nav">
-                    <div className="btn-group" role="group" aria-label="Large button group">
-                      <button className={`btn btn-outline-primary ${categoryId === 'all' ? 'active' : ''}`} type="button" onClick={() => { handleNavClick('all') }}>WebAll</button>
-
-                      {PORTFOLIO_NAV.slice(0, 3).map((navItem) => (
-                        <button key={`nev_${navItem}`} className={`btn btn-outline-primary ${navItem === categoryId ? 'active' : ''}`} type="button" onClick={() => { handleNavClick(navItem) }}>{navItem}</button>
-                      ))}
-                    </div>
+                    <BtnGroupNav isTool navArray={PORTFOLIO} keyCategory={PAGE_KEY_WORD} navOrder={ORDER} setValueCategory={setCategoryId} valueCategory={categoryId} SHOW_ITEM={4} />
                   </div>
                 </nav>
 
                 <div className="portfolio-content">
-
                   {PORTFOLIO_CATEGORY.map((item, index) => {
                     return (
                       <CardInfo card={item} key={`portfolio_${index}`}></CardInfo>
                     )
-                  }
-                  )}
+                  })}
                 </div>
               </div>
             </div>
