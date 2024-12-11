d9f621ae07bb969d06df5eb57ae6e3406505b545
minfan Chen <mingfan1202@gmail.com>
2024-12-12 00:03:08 +0800

    feat: header優化

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './header.scss';
 
 export type NavLeftItemsType = {
@@ -9,31 +9,33 @@ export type NavLeftItemsType = {
   handleClick: () => void,
 };
 export default function Header(props) {
-  const { NAV_LINK, scrollToAnchor } = props;
+  const location = useLocation();
+  const { NAV_LINK } = props;
   const [navId, setNavId] = useState<string>('')
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   return (
-    <header className={isNavCollapsed ? '' : 'isNavCollapsed'}>
+    <header className="isNavCollapsed">
       <nav className="navbar">
         <div className="container-fluid">
 
-          <div className={`collapse collapse-nav fade ${isNavCollapsed ? 'show' : ''}`}>
+          <div className="collapse collapse-nav fade">
             <ul className="navbar-nav navbar-nav-header">
               {NAV_LINK.map((nav) => (
                 <li className='nav-item' key={`nav_${nav.link}`}>
                   <Link
-                    className={`nav-link ${navId === nav.link ? 'is-active' : ''}`}
+                    className={`nav-link ${nav.link === location.pathname ? 'is-active' : ''}`}
                     to={nav.link}
-                  >{nav.name}</Link>
+                  ><span className="material-symbols-outlined">
+                      {nav.icon}
+                    </span>{nav.name}</Link>
                 </li>
               ))}
             </ul>
           </div>
 
 
-          <button className="btn btn-toggle" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" onClick={() => { handleNavCollapse() }}>
+          <button className="btn btn-toggle" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" onClick={() => { }}>
             <span className="material-symbols-outlined">
               menu
             </span>
@@ -42,9 +44,7 @@ export default function Header(props) {
       </nav>
       <div className="offcanvas offcanvas-uiHeader offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><span className="material-symbols-outlined">
-            home
-          </span></h5>
+          <h5 className="offcanvas-title" id="offcanvasNavbarLabel"></h5>
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
@@ -52,9 +52,11 @@ export default function Header(props) {
             {NAV_LINK.map((nav) => (
               <li className='nav-item' key={`nav_${nav.link}`}>
                 <Link
-                  className={`nav-link ${navId === nav.link ? 'is-active' : ''}`}
+                  className={`nav-link ${nav.link === location.pathname ? 'is-active' : ''}`}
                   to={nav.link}
-                >{nav.name}</Link>
+                ><span className="material-symbols-outlined">
+                    {nav.icon}
+                  </span>{nav.name}</Link>
               </li>
             ))}
           </ul>
