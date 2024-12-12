b3f3eba869814b40aaadaa99508c87ac7a220af6
minfan Chen <mingfan1202@gmail.com>
2024-12-13 02:26:32 +0800

    feat: 作品內頁

---
+++ src/page/PortfolioPage/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, useOutletContext, useLocation } from 'react-router-dom'
+import Breadcrumb from "@components/common/BreadCrumb";
+import BreadcrumbItem from "@components/common/BreadCrumb/BreadcrumbItem";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAtom } from '@fortawesome/free-solid-svg-icons'
+import { DATABASEProps, portfolioProps } from '@typeTS/dataBase'
+
+type dataType = {
+  myDataBase: DATABASEProps,
+  NAV_LINK: {
+    name: string,
+    link: string,
+    icon: string
+  }[],
+}
+
+export default function PortfolioPage() {
+  const { portfolioId } = useParams();
+  const { myDataBase, NAV_LINK } = useOutletContext<dataType>();
+  const PORTFOLIO_BASE = Object.values(myDataBase.portfolio || {})
+  const PORTFOLIO_BASE_ITEM: portfolioProps = PORTFOLIO_BASE.find((item) => item.id === portfolioId)
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  console.log(PORTFOLIO_BASE_ITEM)
+
+  return (
+    <div className="page">
+      <div className="container-xl">
+        <div className="row">
+          <div className="col"><Breadcrumb>
+            <BreadcrumbItem itemIcon={NAV_LINK[0].icon} itemLink={NAV_LINK[0].link} />
+            <BreadcrumbItem itemLink={PORTFOLIO_BASE_ITEM?.title} itemName={PORTFOLIO_BASE_ITEM?.category} />
+            <BreadcrumbItem itemName={PORTFOLIO_BASE_ITEM?.title} itemActive />
+          </Breadcrumb>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
