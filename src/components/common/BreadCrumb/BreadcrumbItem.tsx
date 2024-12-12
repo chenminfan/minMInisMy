59f58f0ad7ec3d244d24a66bc33b8fbd2deb1140
minfan Chen <mingfan1202@gmail.com>
2024-12-13 02:26:06 +0800

    feat: 麵包屑元件化

---
+++ src/components/common/BreadCrumb/BreadcrumbItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {
+  itemLink?: string,
+  itemName?: string,
+  itemIcon?: string,
+  itemActive?: boolean,
+}
+
+export default function BreadcrumbItem(props: Props) {
+  const { itemLink, itemName, itemIcon, itemActive = false } = props
+  return (
+    <li className="breadcrumb-item">
+      <a className={`${itemActive ? 'text-primary' : 'text-muted'}`} href={itemLink} role="link" aria-label="breadcrumb-link">
+        {itemIcon ? <span className="material-symbols-outlined">
+          {itemIcon}
+        </span> : itemName}</a>
+    </li >
+  )
+}
