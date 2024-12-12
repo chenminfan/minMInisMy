b62044450d4282984f515fda4aa232899fb3beae
minfan Chen <mingfan1202@gmail.com>
2024-12-12 23:45:07 +0800

    feat: 優化所有圖片狀態及無圖片時

--- src/components/common/LazyLoadImage.tsx
+++ src/components/common/LazyLoadImage.tsx
@@ -1,5 +1,6 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import PlaceholderImage from "../../assets/image/background/background-02.png";
 
 type LazyLoadImgType = {
   className: string,
@@ -10,15 +11,14 @@ type LazyLoadImgType = {
   height?: number,
 }
 const LazyLoadImg = (props: LazyLoadImgType) => {
-  const { alt, src, width = 800, height = 500, className, placeholdersrc } = props
+  const { alt, src, className, placeholdersrc } = props
   return (
     <LazyLoadImage
       alt={alt}
       src={src} // use normal <img> attributes as props
       className={className}
-      placeholdersrc={placeholdersrc}
+      placeholderSrc={PlaceholderImage}
       effect="blur"
-      visibleByDefault={src === 'https://plus.unsplash.com/premium_photo-1707931342773-6eb567d4fd21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z3JleXxlbnwwfHwwfHx8MA%3D%3D'}
     />
   )
 }
