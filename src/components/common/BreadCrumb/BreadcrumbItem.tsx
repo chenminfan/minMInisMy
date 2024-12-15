7218b1133b03c61b44e2ff4e273248be06625afb
minfan Chen <mingfan1202@gmail.com>
2024-12-16 06:12:56 +0800

    feat: 作品內頁分類路由link

--- src/components/common/BreadCrumb/BreadcrumbItem.tsx
+++ src/components/common/BreadCrumb/BreadcrumbItem.tsx
@@ -5,15 +5,18 @@ type Props = {
   itemName?: string,
   itemIcon?: object | any,
   itemActive?: boolean,
+  handleClick?: (string) => void
 }
 
 export default function BreadcrumbItem(props: Props) {
-  const { itemLink = '', itemName = '', itemIcon, itemActive = false } = props
+  const { itemLink = '', itemName = '', itemIcon, itemActive = false, handleClick = () => { } } = props
   return (
-    <li className="breadcrumb-item">
+    <li className="breadcrumb-item" onClick={handleClick}>
       {itemLink ? (<a className={`${itemActive ? 'text-primary' : 'text-muted'}`} href={itemLink} role="link" aria-label="breadcrumb-link">
         <span className='breadcrumb-icon me-2'>{itemIcon && itemIcon}</span>{itemName && itemName}</a>) : (<span className={`${itemActive ? 'text-primary' : 'text-muted'}`} role="link" aria-label="breadcrumb-link">
-          <span className='breadcrumb-icon me-2'>{itemIcon && itemIcon}</span>{itemName && itemName}</span>)}
+          <span className='breadcrumb-icon me-2'>{itemIcon && itemIcon}</span>{itemName && itemName}
+        </span>
+      )}
     </li >
   )
 }
